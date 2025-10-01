a0ce1c408b2f756d77b429f066e85ddca8c6bdb3
logbyjungle <lucamiao072@gmail.com>
2025-10-01 18:44:39 +0200

    driver is now always running

--- main.py
+++ main.py
@@ -1,28 +1,13 @@
-from bot import translate_and_store, stopdriver, startdriver
+from bot import translate_and_store, startdriver
 from flask import Flask, render_template
-import threading
-import time
 
 app = Flask(__name__)
-last_request_time = time.time()
 
 with open("languages.txt","r") as file:
     langs = eval(file.read())
 
-def monitor_inactivity():
-    global last_request_time
-    while True:
-        time.sleep(10)
-        if time.time() - last_request_time > 600:
-            print("stopped driver after 10m")
-            stopdriver()
-            last_request_time = time.time()
-
 @app.before_request
 def driverstart():
-    global last_request_time
-    last_request_time = time.time()
-    print("starting driver due to page loading")
     startdriver()
 
 @app.route("/")
@@ -45,5 +30,4 @@ def page(lang, chapter):
     return render_template("chapter.html",chapter=chapter,content=content)
 
 if __name__ == '__main__':
-    threading.Thread(target=monitor_inactivity, daemon=True).start()
     app.run(debug=True,use_reloader=False,host="0.0.0.0")
