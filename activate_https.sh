7d51fae144228aa427f1feaae408dbd60197c4e1
logbyjungle <lucamiao072@gmail.com>
2025-11-04 21:05:16 +0100

    added chapters

--- activate_https.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-certbot certonly --non-interactive --agree-tos --email "$EMAIL" --preferred-challenges dns --authenticator dns-duckdns --dns-duckdns-token "$TOKEN" --dns-duckdns-propagation-seconds 60 -d "$DOMAIN"
-cd etc/nginx/conf.d/
-mv nginx.conf nginx.conf.disabled
-mv nginx.conf.https nginx.conf
-nginx -s reload
