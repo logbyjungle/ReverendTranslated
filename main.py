f61992638c8c2d1c3f9447a8b2ed4fd03468a987
logbyjungle <lucamiao072@gmail.com>
2025-11-12 15:51:44 +0100

    moved request validation to a function decorator

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from bot import translatewhole, startdriver
 from flask import Flask, Response, redirect, render_template, stream_template, url_for
 import subprocess
 import argparse
+from functools import wraps
 
 app = Flask(__name__)
 
@@ -28,31 +29,34 @@ args,_ = parser.parse_known_args()
 if not args.nodriver:
     startdriver(args.headful)
 
+def validity_check(func):
+    @wraps(func)
+    def wrapper(lang,chapter):
+        if chapter not in [str(i) for i in range (1,2335)]:
+            if args.verbose: print(f"DEBUG: selected chapter not in range 1-2334")
+            return redirect(url_for("home"))
+        elif lang not in langs.values():
+            if args.verbose: print(f"DEBUG: selected language not in languages list")
+            return redirect(url_for("home"))
+        return func(lang,chapter)
+    return wrapper
+
 @app.route("/",methods=["GET"])
 def home():
     return render_template("main.html", mapping=langs, version=get_version())
 
 @app.route("/api/<lang>/<chapter>",methods=["GET"])
+@validity_check
 def call(lang,chapter):
-    if chapter not in [str(i) for i in range (1,2335)]:
-        return redirect(url_for("home"))
-
-    if lang not in langs.values():
-        return redirect(url_for("home"))
-
     def generate():
         for paragraph in translatewhole(chapter,lang,args):
             yield ('\n'.join(paragraph) + '\n').encode('UTF-8')
     return Response(generate(),mimetype='text/plain',direct_passthrough=True)
 
 @app.route("/<lang>/<chapter>",methods=["GET"])
+@validity_check
 def page(lang, chapter):
-    if chapter not in [str(i) for i in range (1,2335)]:
-        return redirect(url_for("home"))
-
-    if lang not in langs.values():
-        return redirect(url_for("home"))
-
+    _ = lang # suppresses unused lang warning
     return stream_template("chapter.html",chapter=chapter)
 
 if __name__ == '__main__':
