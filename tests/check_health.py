5a26d36e3a477594e7dbac14057337541f655b1e
logbyjungle <lucamiao072@gmail.com>
2025-11-25 20:14:49 +0100

    added debug print statements

--- tests/check_health.py
+++ tests/check_health.py
@@ -4,9 +4,12 @@ import urllib.request
 
 for _ in range(30):
     try:
-        if urllib.request.urlopen("http://localhost:5000/health",timeout=2).getcode() == 200:
+        code = urllib.request.urlopen("http://localhost:5000/health",timeout=2).getcode()
+        if code == 200:
             sys.exit(0)
-    except Exception: pass
+        else:
+            print(f"GOT STATUS CODE {code}")
+    except Exception as e: print(e)
     time.sleep(2)
 
 sys.exit(1)
