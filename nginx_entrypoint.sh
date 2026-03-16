2cd9788234c1e5e42881d4736159129ea529e8e9
logbyjungle <lucamiao072@gmail.com>
2026-03-16 19:21:07 +0100

    status code check removed

--- nginx_entrypoint.sh
+++ nginx_entrypoint.sh
@@ -5,23 +5,20 @@ CURRENT_URL=""
 
 while true; do
     URL=$(curl -s "https://raw.githubusercontent.com/$REPO/tunnel/address.txt")
-    HTTP_STATUS=$(curl -s -o /dev/null -w "%{http_code}" "$URL")
-    if [ "$HTTP_STATUS" = "200" ]; then
-        if [ "$CURRENT_URL" != "$URL" ]; then
-            export TUNNEL_URL="$URL"
-            envsubst '${TUNNEL_URL}' < "/nginx.conf.template" > "/nginx.conf.template2"
-            export TUNNEL_HOST=$(echo $TUNNEL_URL | awk -F/ '{print $3}')
-            envsubst '${TUNNEL_HOST}' < "/nginx.conf.template2" > "/etc/nginx/conf.d/default.conf"
+    if [ "$CURRENT_URL" != "$URL" ]; then
+        export TUNNEL_URL="$URL"
+        envsubst '${TUNNEL_URL}' < "/nginx.conf.template" > "/nginx.conf.template2"
+        export TUNNEL_HOST=$(echo $TUNNEL_URL | awk -F/ '{print $3}')
+        envsubst '${TUNNEL_HOST}' < "/nginx.conf.template2" > "/etc/nginx/conf.d/default.conf"
 
-            if [ -z "$NGINX_STARTED" ]; then
-                nginx -g 'daemon off;' &
-                NGINX_STARTED=1
-            else
-                nginx -s reload
-            fi
-            CURRENT_URL="$URL"
+        if [ -z "$NGINX_STARTED" ]; then
+            nginx -g 'daemon off;' &
+            NGINX_STARTED=1
+        else
+            nginx -s reload
         fi
-    fi
+        CURRENT_URL="$URL"
+        fi
     sleep 10
 done
 
