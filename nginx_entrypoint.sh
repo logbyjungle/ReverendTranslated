6e68220c3d9dadbda70f03d6250d69c27cf92038
logbyjungle <lucamiao072@gmail.com>
2026-03-17 22:52:25 +0100

    possibly fixing silent fail forgot sleep and variable name

--- nginx_entrypoint.sh
+++ nginx_entrypoint.sh
@@ -7,16 +7,23 @@ while true; do
     if [ "$CURRENT_URL" != "$URL" ]; then
         export TUNNEL_URL="$URL"
         envsubst '${TUNNEL_URL}' < "/nginx.conf.template" > "/nginx.conf.template2"
-        export TUNNEL_HOST=$(echo $TUNNEL_URL | awk -F/ '{print $3}')
+        export TUNNEL_HOST=$(echo $URL | awk -F/ '{print $3}')
         envsubst '${TUNNEL_HOST}' < "/nginx.conf.template2" > "/etc/nginx/conf.d/default.conf"
 
         if [ -z "$NGINX_STARTED" ]; then
             nginx -g 'daemon off;' &
             NGINX_STARTED=1
         else
-            nginx -s reload
+            for _ in $(seq 1 10); do
+                if nginx -t && nginx -s reload; then
+                    echo "successful reload of ${URL}"
+                else
+                    echo "unsuccessful reload of ${URL}"
+                fi
+                sleep 10
+            done
         fi
         CURRENT_URL="$URL"
-        fi
+    fi
     sleep 10
 done
